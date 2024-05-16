51236c84b7176a486adad73eb210723077a21bed
Liirus91 <work.liirus@gmail.com>
2024-05-16 09:26:16 +0300

    feat: create Header compnent

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header';
+
+type Props = { children: React.ReactNode };
+
+const DashnoardLayouts = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+      <main className="px-3 lg:px-14">{children}</main>
+    </>
+  );
+};
+
+export default DashnoardLayouts;
