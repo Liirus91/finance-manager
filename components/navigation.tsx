dc482971f52c669db1d6a39696de187935877fe9
Liirus91 <work.liirus@gmail.com>
2024-05-16 09:43:27 +0300

    feat: create routes in Navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,7 +1,21 @@
 'use client';
 
+const routes = [
+  { href: '/', label: 'Overview' },
+  { href: '/transactions', label: 'Transactions' },
+  { href: '/accounts', label: 'Accounts' },
+  { href: '/categories', label: 'Categories' },
+  { href: '/settings', label: 'Settings' },
+];
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
+      {routes.map((route) => (
+        <p>{route.label}</p>
+      ))}
+    </nav>
+  );
 };
 
 export default Navigation;
