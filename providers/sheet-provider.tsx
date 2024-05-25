c42221ef7dd664d5a075acac88d29ae5416bf3b4
Liirus91 <work.liirus@gmail.com>
2024-05-25 08:31:59 +0300

    feat: add mounted state in SheetProvider

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,8 +1,15 @@
 'use client';
 
 import NewAccountSheet from '@/features/components/new-account-sheet';
+import { useMountedState } from 'react-use';
 
 const SheetProvider = () => {
+  const isMounted = useMountedState();
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <>
       <NewAccountSheet />
