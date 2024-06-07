ff8eb32c0c18a5960cd5917361351b4a3ca87490
Liirus91 <work.liirus@gmail.com>
2024-06-07 09:13:03 +0300

    feat: add categories sheets in SheetProvider

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -3,6 +3,8 @@
 import NewAccountSheet from '@/features/accounts/components/new-account-sheet';
 import EditAccountSheet from '@/features/accounts/components/edit-account-sheet';
 import { useMountedState } from 'react-use';
+import NewCategorySheet from '@/features/categories/components/new-category-sheet';
+import EditCategorySheet from '@/features/categories/components/edit-category-sheet';
 
 const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -15,6 +17,9 @@ const SheetProvider = () => {
     <>
       <NewAccountSheet />
       <EditAccountSheet />
+
+      <NewCategorySheet />
+      <EditCategorySheet />
     </>
   );
 };
