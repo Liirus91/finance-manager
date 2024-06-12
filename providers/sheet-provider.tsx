98acc40faf2dd91a8b66e0270a7e0205170cfd84
Liirus91 <work.liirus@gmail.com>
2024-06-12 08:09:21 +0300

    feat: use NewTransactionSheet in SheetProvider

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -5,6 +5,7 @@ import EditAccountSheet from '@/features/accounts/components/edit-account-sheet'
 import { useMountedState } from 'react-use';
 import NewCategorySheet from '@/features/categories/components/new-category-sheet';
 import EditCategorySheet from '@/features/categories/components/edit-category-sheet';
+import NewTransactionSheet from '@/features/transactions/components/new-transaction-sheet';
 
 const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -20,6 +21,8 @@ const SheetProvider = () => {
 
       <NewCategorySheet />
       <EditCategorySheet />
+
+      <NewTransactionSheet />
     </>
   );
 };
