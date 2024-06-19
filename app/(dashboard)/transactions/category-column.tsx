052b92256e2086a59026d7f7650997ac23d2530e
Liirus91 <work.liirus@gmail.com>
2024-06-19 09:59:18 +0300

    feat: open transaction form if deleted category was clicked

--- app/(dashboard)/transactions/category-column.tsx
+++ app/(dashboard)/transactions/category-column.tsx
@@ -1,4 +1,5 @@
 import { useOpenCategory } from '@/features/categories/hooks/use-open-category';
+import { useOpenTransaction } from '@/features/transactions/hooks/use-open-transaction';
 import { cn } from '@/lib/utils';
 import { TriangleAlert } from 'lucide-react';
 
@@ -14,10 +15,13 @@ export const CategoryColumn = ({
   categoryId,
 }: CategoryColumnProps) => {
   const { onOpen: onOpenCategory } = useOpenCategory();
+  const { onOpen: onOpentransaction } = useOpenTransaction();
 
   const onClick = () => {
     if (categoryId) {
       onOpenCategory(categoryId);
+    } else {
+      onOpentransaction(id);
     }
   };
 
