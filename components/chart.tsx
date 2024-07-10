1b6293308fa9d0b6de31d7abcf1b16cb33d0c115
Liirus91 <work.liirus@gmail.com>
2024-07-10 08:44:51 +0300

    feat: create Chart component

---
+++ components/chart.tsx
@@ -0,0 +1,6 @@
+type Props = {
+  data?: { date: string; income: number; expenses: number }[];
+};
+export const Chart = ({ data = [] }: Props) => {
+  return <div>Chart</div>;
+};
--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useGetSummary } from '@/features/summary/api/use-get-summary';
+import { Chart } from './chart';
 
 export const DataCharts = () => {
   const { data, isLoading } = useGetSummary();
@@ -8,5 +9,12 @@ export const DataCharts = () => {
   if (isLoading) {
     return <div>Loading...</div>;
   }
-  return <div>DataCharts</div>;
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
+      <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+        <Chart data={data?.days} />
+      </div>
+    </div>
+  );
 };
